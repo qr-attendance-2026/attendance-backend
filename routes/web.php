802f0374b1ac8563ce97ccce7db07f992c659889
Trang Doan <doantrangltt@gmail.com>
2026-03-14 19:38:36 +0700

    feat: Add /setup-db route to run migrations from Cloud Run

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -13,5 +12,12 @@ Route::get('/', function () {
 Route::get('/users', [UserController::class,'index']);
 Route::get('/users/{id}', [UserController::class,'show']);
 Route::post('/users', [UserController::class,'store']);
+
+// Temporary route to migrate the database from Cloud Run
+Route::get('/setup-db', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return 'Quá trình tạo bảng (Migrate) đã hoàn tất!';
+});
+
 Route::put('/users/{id}', [UserController::class,'update']);
 Route::delete('/users/{id}', [UserController::class,'destroy']);
